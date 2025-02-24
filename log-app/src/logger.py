a06c3e7896b7e2dd84d73e2d6ad366800600654c
Uwe Helm (bcgov) <uwe.helm@gov.bc.ca>
2025-02-24 10:52:13 -0800

    adding json output

--- log-app/src/logger.py
+++ log-app/src/logger.py
@@ -4,13 +4,14 @@ import sys
 import logging
 import time
 from random import randint
+from pythonjsonlogger.json import JsonFormatter
 
 import cowsay
 
 logger = logging.getLogger('')
 logger.setLevel(logging.NOTSET)
 sh = logging.StreamHandler(sys.stdout)
-eh = logging.StreamHandler(sys.stderr)
+#eh = logging.StreamHandler(sys.stderr)
 formatter_stdout = logging.Formatter( \
     '[%(asctime)s] STDOUT %(levelname)s \
     [%(filename)s.%(funcName)s:%(lineno)d] %(message)s', \
@@ -20,22 +21,24 @@ formatter_stderr = logging.Formatter(\
     '[%(asctime)s] STDERR %(levelname)s \
     [%(filename)s.%(funcName)s:%(lineno)d] %(message)s', \
     datefmt='%a, %d %b %Y %H:%M:%S')
-sh.setFormatter(formatter_stdout)
-eh.setFormatter(formatter_stderr)
+#sh.setFormatter(formatter_stdout)
+#eh.setFormatter(formatter_stderr)
+sh.setFormatter(JsonFormatter())
 sh.setLevel(logging.DEBUG)
-eh.setLevel(logging.WARNING)
-logger.addHandler(eh)
+#eh.setLevel(logging.WARNING)
 logger.addHandler(sh)
+#logger.addHandler(eh)
+#logger.addHandler(sh)
 
 def cow():
     return cowsay.cow("Moooo...")
 
 def test_logger():
-    logger.info("Hello info")
-    logger.critical("Hello critical")
-    logger.warning("Hello warning")
-    logger.error("Hello error")
-    logger.debug("Hello debug")
+    logger.info("Hello", extra={"level": "info"})
+    logger.critical("Hello", extra={"level": "critical"})
+    logger.warning("Hello", extra={"level": "warning"})
+    logger.error("Hello", extra={"level": "error"})
+    logger.debug("Hello", extra={"level": " debug"})
 
 if __name__ == "__main__":
     cow()
